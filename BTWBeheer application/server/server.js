0412a5b1662d48d873e42ee1744145b4c4bb6e52
Midas <64684902+midasn74@users.noreply.github.com>
2023-09-14 16:21:39 +0200

    setup docker

--- BTWBeheer application/server/server.js
+++ BTWBeheer application/server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001; // Choose a suitable port
-
-require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_DATABASE;
 
+if (!dbHost || !dbUser || !dbPassword || !dbName) {
+  throw new Error('Environment variables not set');
+}
+
 // Add your routes and middleware here
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port} database: ${dbHost}`);
+  console.log(`Server is running on port ${port}`);
 });
