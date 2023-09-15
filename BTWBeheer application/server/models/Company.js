177af8e8c96aab75f6e642c681c49ee7f994ecae
Midas <64684902+midasn74@users.noreply.github.com>
2023-09-15 10:58:56 +0200

    add company sequelize object

---
+++ BTWBeheer application/server/models/Company.js
@@ -0,0 +1,66 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Import your Sequelize configuration
+
+const Company = sequelize.define('Company', {
+    // Define the table columns and their data types
+    business_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    business_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    contact_mail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    contact_phone_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    bank_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    kvk_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    vat_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    vat_declaration_interval: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    postal_code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    country: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    default_payment_term_days: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    default_quotation_validity_days: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+
+module.exports = Company;
