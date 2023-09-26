64583d530fdaca3bf22a460267633073bff3d137
Midas <64684902+midasn74@users.noreply.github.com>
2023-09-26 11:07:08 +0200

    add validation for patch invoice #2

--- BTWBeheer application/server/routes/Validation/invoicePatchValidation.js
+++ BTWBeheer application/server/routes/Validation/invoicePatchValidation.js
@@ -1,8 +1,23 @@
 const { body, validationResult } = require('express-validator');
 
 const validateInvoice = [
+    body('company_id')
+        .isNumeric().withMessage('Company id must be a number'),
+
     body('relation_id')
-        .isNumeric().withMessage('Relation must be a number'),
+        .isNumeric().withMessage('Relation id must be a number'),
+
+    body('invoice_description')
+        .isString().withMessage('Invoice description must be a string'),
+    
+    body('creation_date')
+        .isDate().withMessage('Creation date must be a date'),
+    
+    body('due_date')
+        .isDate().withMessage('Due date must be a date'),
+
+    body('payment_term_days')
+        .isInt({ min: 1, max: 365 }).withMessage('A valid default payment term is required (1-365) days'),
         
     // After defining validation rules, handle the route logic
     (req, res, next) => {
