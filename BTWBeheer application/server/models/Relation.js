61dad6b146688eac77711ce5d5cab03c03283373
Midas <64684902+midasn74@users.noreply.github.com>
2023-09-26 10:52:17 +0200

    Finish relation model sqlz #4

--- BTWBeheer application/server/models/Relation.js
+++ BTWBeheer application/server/models/Relation.js
@@ -16,6 +16,60 @@ module.exports = (sequelize) => {
                 key: 'company_id',
             },
         },
+        relation_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_contact: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validator: {
+                isEmail: true,
+            },
+        },
+        relation_phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validator: {
+                isNumeric: true,
+            },
+        },
+        relation_address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_postal_code: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_country: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_kvk_number: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_vat_number: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_iban: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        relation_salutation: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     });
 
     return Relation;
