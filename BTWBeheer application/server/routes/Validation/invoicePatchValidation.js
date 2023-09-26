ab3bb4f89c88b7301c645ddbd4361942bde83c55
Midas <64684902+midasn74@users.noreply.github.com>
2023-09-26 10:53:37 +0200

    setup validation and route for patch and delete invoice #2 #1

---
+++ BTWBeheer application/server/routes/Validation/invoicePatchValidation.js
@@ -0,0 +1,20 @@
+const { body, validationResult } = require('express-validator');
+
+const validateInvoice = [
+    body('relation_id')
+        .isNumeric().withMessage('Relation must be a number'),
+        
+    // After defining validation rules, handle the route logic
+    (req, res, next) => {
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // If validation passes, proceed to the route handler
+        next();
+    }
+];
+
+module.exports = validateInvoice;
--- BTWBeheer application/server/routes/invoices.js
+++ BTWBeheer application/server/routes/invoices.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const { Invoice } = require('../sequelize');
 const authenticateToken = require('./Authentication/tokenAuthentication');
 const validateInvoice = require('./Validation/invoiceValidation');
+const validateInvoicePatch = require('./Validation/invoicePatchValidation');
+const { Company } = require('../sequelize');
 
 // Route to create a new invoice
-router.post('/', validateInvoice, async (req, res) => {
+router.post('/', [validateInvoice, authenticateToken], async (req, res) => {
     try {
         const {  } = req.body;
     
@@ -21,23 +23,96 @@ router.post('/', validateInvoice, async (req, res) => {
     }
 });
 
-router.get('/:invoiceId', authenticateToken, async (req, res) => {
+router.patch('/:invoiceId', [validateInvoicePatch, authenticateToken], async (req, res) => {
     try {
         const invoiceId = parseInt(req.params.invoiceId, 10);
 
-        // Check if the authenticated company has permission to access the requested company's data
+        // Check if the authenticated company has permission to access the requested invoice's data
         if (req.AuthCompanyId !== Invoice.findByPk(invoiceId).company_id) {
             return res.status(403).json({ error: 'Access denied' });
         }
 
-        // Fetch the company data
+        // Fetch the invoice data
         const invoice = await Invoice.findByPk(invoiceId);
         if (!invoice) {
             return res.status(404).json({ error: 'Invoice not found' });
         }
 
-        // Return the company data
+        // Patch the invoice
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+router.get('/:invoiceId', authenticateToken, async (req, res) => {
+    try {
+        const invoiceId = parseInt(req.params.invoiceId, 10);
+
+        // Check if the authenticated company has permission to access the requested invoice's data
+        if (req.AuthCompanyId !== Invoice.findByPk(invoiceId).company_id) {
+            return res.status(403).json({ error: 'Access denied' });
+        }
+
+        // Fetch the invoice data
+        const invoice = await Invoice.findByPk(invoiceId);
+        if (!invoice) {
+            return res.status(404).json({ error: 'Invoice not found' });
+        }
+
+        // Return the invoice data
         res.json(invoice);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+router.delete('/:invoiceId', authenticateToken, async (req, res) => {
+    try {
+        const invoiceId = parseInt(req.params.invoiceId, 10);
+
+        // Check if the authenticated company has permission to access the requested invoice's data
+        if (req.AuthCompanyId !== Invoice.findByPk(invoiceId).company_id) {
+            return res.status(403).json({ error: 'Access denied' });
+        }
+
+        // Fetch the invoice data
+        const invoice = await Invoice.findByPk(invoiceId);
+        if (!invoice) {
+            return res.status(404).json({ error: 'Invoice not found' });
+        }
+
+        // Delete the invoice
+        await invoice.destroy();
+
+        // Respond with 204 No Content
+        res.status(204).end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+router.get('/company/:companyId', authenticateToken, async (req, res) => {
+    try {
+        const companyId = parseInt(req.params.companyId, 10);
+
+        // Check if the authenticated company has permission to access the requested company's data
+        if (req.AuthCompanyId !== companyId) {
+            return res.status(403).json({ error: 'Access denied' });
+        }
+
+        const company = await Company.findByPk(req.AuthCompanyId);
+
+        // Fetch all invoices for the company
+        const invoices = await Invoice.findAll({ where: { company_id: companyId } });
+        if (!invoices || invoices.length === 0) {
+            return res.status(404).json({ error: `No invoices found for company: ${company.company_name} (${company.company_id})` });
+        }
+
+        // Return the invoices data
+        res.json(invoices);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server error' });
