8d53026d9590f6f5f725ee2b96dd56a6198b26a1
Midas <64684902+midasn74@users.noreply.github.com>
2023-10-02 17:15:22 +0200

    reformat

--- BTWBeheer application/client/src/components/Relations.js
+++ BTWBeheer application/client/src/components/Relations.js
@@ -17,73 +17,73 @@ const Dashboard = () => {
     }, []);
 
     return (
-    <div>
-        <FullNavbar company={company}/>
+        <div>
+            <FullNavbar company={company}/>
 
-        {/*Make a relation creator*/}
-        <Container>
-            <Row className="align-items-center">
-                <Col>
-                    <h1>Relations</h1>
-                </Col>
-                <Col className="d-flex justify-content-end">
-                    <Button variant="success">+ Relation</Button>
-                </Col>
-            </Row>
-        </Container>
+            {/*Make a relation creator*/}
+            <Container>
+                <Row className="align-items-center">
+                    <Col>
+                        <h1>Relations</h1>
+                    </Col>
+                    <Col className="d-flex justify-content-end">
+                        <Button variant="success">+ Relation</Button>
+                    </Col>
+                </Row>
+            </Container>
 
-        {/*Make a relation searching module*/}
+            {/*Make a relation searching module*/}
 
-        {/*Relations table*/}
-        {relations == null ? 
-            <Alert variant="info">
-                <Alert.Heading>Hey, it seems you have not yet made a relation.</Alert.Heading>
-                <p>
-                    To create your first relation, please click on the plus icon in the top right corner!
-                </p>
-            </Alert>
-        : 
-            <Table striped hover responsive variant="dark">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Contact person</th>
-                        <th>Phone number</th>
-                        <th>E-mail</th>
-                        <th>Address</th>
-                        <th>Postal code</th>
-                        <th>City</th>
-                        <th>Country</th>
-                        <th>KVK number</th>
-                        <th>VAT number</th>
-                        <th>IBAN number</th>
-                        <th>Salutation</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        {relations.map(relation => (  
-                            <tr>
-                                <td>{relation.relation_id}</td>  
-                                <td>{relation.relation_name}</td>  
-                                <td>{relation.relation_contact}</td>  
-                                <td>{relation.relation_email}</td>  
-                                <td>{relation.relation_phone}</td>  
-                                <td>{relation.relation_address}</td>  
-                                <td>{relation.relation_postal_code}</td>  
-                                <td>{relation.relation_city}</td>  
-                                <td>{relation.relation_country}</td>  
-                                <td>{relation.relation_kvk_number}</td>  
-                                <td>{relation.relation_vat_number}</td>  
-                                <td>{relation.relation_iban}</td>  
-                                <td>{relation.relation_salutation}</td>  
-                            </tr>
-                        ))}  
-                </tbody>
-            </Table>
-        }
-    </div>
+            {/*Relations table*/}
+            {relations == null ? 
+                <Alert variant="info">
+                    <Alert.Heading>Hey, it seems you have not yet made a relation.</Alert.Heading>
+                    <p>
+                        To create your first relation, please click on the plus icon in the top right corner!
+                    </p>
+                </Alert>
+            : 
+                <Table striped hover responsive variant="dark">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Contact person</th>
+                            <th>Phone number</th>
+                            <th>E-mail</th>
+                            <th>Address</th>
+                            <th>Postal code</th>
+                            <th>City</th>
+                            <th>Country</th>
+                            <th>KVK number</th>
+                            <th>VAT number</th>
+                            <th>IBAN number</th>
+                            <th>Salutation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {relations.map(relation => (  
+                                <tr>
+                                    <td>{relation.relation_id}</td>  
+                                    <td>{relation.relation_name}</td>  
+                                    <td>{relation.relation_contact}</td>  
+                                    <td>{relation.relation_email}</td>  
+                                    <td>{relation.relation_phone}</td>  
+                                    <td>{relation.relation_address}</td>  
+                                    <td>{relation.relation_postal_code}</td>  
+                                    <td>{relation.relation_city}</td>  
+                                    <td>{relation.relation_country}</td>  
+                                    <td>{relation.relation_kvk_number}</td>  
+                                    <td>{relation.relation_vat_number}</td>  
+                                    <td>{relation.relation_iban}</td>  
+                                    <td>{relation.relation_salutation}</td>  
+                                </tr>
+                            ))}  
+                    </tbody>
+                </Table>
+            }
+        </div>
     );
-  };
+};
  
 export default Dashboard;
